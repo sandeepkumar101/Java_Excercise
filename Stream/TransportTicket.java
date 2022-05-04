dd3e5d9016110f17c5f6de2774dcf675601cebb2
skgurjar <sandeep.sandy1984@gmail.com>
2022-05-03 19:51:39 -0500

    More Algos

--- Stream/TransportTicket.java
+++ Stream/TransportTicket.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * You want to buy public transport tickets for some upcoming days. You know exactly the days on which you will be traveling. There are three types of ticket:
 
@@ -40,5 +44,99 @@ array A is sorted in increasing order;
 the elements of A are all distinct.
  */
 public class TransportTicket {
-    
+
+    public static void main(String[] args) {
+        int[] days = {1,4,6,7,8,20}, costs = {2,7,15};
+        System.out.println(mincostTickets1(days, costs));
+    }
+    public static int mincostTickets(int[] days, int[] costs) {   
+        int[] durations = new int[]{1, 7, 30};
+        int[] dp = new int[days.length + 1];
+        List<Integer> list = new ArrayList<>();
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 1; i <= days.length; i++){ 
+            for(int k = 0; k < 3; k++){
+                int next = i;
+                while(next >= 1 && days[next - 1] > days[i - 1] - durations[k]){
+                    next--;
+                }
+                if(dp[i]>dp[next] + costs[k])
+                {
+                    list.add(costs[k]);
+                }
+                dp[i] = Math.min(dp[i], dp[next] + costs[k]);
+
+            }   
+        }
+        System.out.println(list.toString());
+        return dp[days.length];
+    }
+
+
+    public static int mincostTickets1(int[] days, int[] costs) {   
+        int n = days.length;
+        int[] duration = {1,7,30};
+        int[] dp = new int[n+1];
+        int[] dpTicket = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        List<Integer> list = new ArrayList<>();
+        List<Integer> tickets = new ArrayList<>();
+        for(int i = 0; i <= n ; i++)
+        {
+            //int[] minCost = new int[3];
+            //int min  = 0;
+            for(int k = 0; k<costs.length ; k++)
+            {
+                int next = i;
+                while(next >= 1 && days[next-1] > days[i-1]-duration[k] )
+                {
+                    next--;
+                }
+                //minCost[k]=dp[next] + costs[k];
+                dp[i] = Math.min(dp[i], dp[next] + costs[k]);
+            }
+            //list.add(Math.min(minCost[0], Math.min(minCost[1],minCost[2])));
+            
+            
+        }
+        
+        dpTicket = ticketByday(dp,costs,dpTicket);
+        System.out.println(list.toString());
+        return dp[days.length];
+    }
+
+    public static int[] ticketByday(int[] daysCost, int[] costs, int[] dpTicket)
+    {
+        for(int i = daysCost.length-1; i>0; i--)
+            {
+            int totalCost = daysCost[i];
+            for(int k = costs.length-1; k >=0; k--)
+            {
+                
+                if(totalCost%costs[k]==0)
+                {
+                    totalCost = costs[k];
+                  /*  dpTicket[i]=costs[k];
+                    int j = i;
+                    int sum = dpTicket[i];
+                    while(j>0 && sum>0)
+                    {
+                        sum -=dpTicket[j]-dpTicket[j-1];
+                        dpTicket[j]=0;
+                        j--;
+                    }
+                    dpTicket[j+1]=costs[k];*/
+                    break;
+                }
+                else {
+                    totalCost = totalCost%costs[k];
+                }
+
+            }
+        }
+        return dpTicket;
+    }
+
 }
