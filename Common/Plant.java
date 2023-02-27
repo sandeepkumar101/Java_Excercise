e726826636652f21001edae9b9d5eb02627bbfad
skgurjar <sandeep.sandy1984@gmail.com>
2023-02-27 08:18:19 -0600

    Update

--- Common/Plant.java
+++ Common/Plant.java
@@ -1,8 +1,13 @@
 package Common;
+
+import java.util.Arrays;
+
 public class Plant {
     public static void main(String[] args) {
-        int[] height = { 4, 5, 3, 7, 2, 1 };
-        System.out.println(solution(height));
+        int[][] height = { { 4, 5, 3, 7, 2, 1, 9 },
+                { 6, 4, 5, 3, 7, 2, 1, 9 },
+                { 4, 5, 3, 7, 2, 1 } };
+        Arrays.asList(height).forEach(e -> System.out.println(solution1(e)));
     }
 
     public static int solution(int[] A) {
@@ -25,10 +30,6 @@ public class Plant {
                     pick++;
                 }
             }
-
-            // if (i == 1 && !checkPickOrVelly(A[i - 1], A[i], A[i + 1])) {
-            // removeCount++;
-            // } else {
             if (checkPickOrVelly(A[i - 2], A[i], A[i + 1])) {
                 removeCount++;
             } else if (checkPickOrVelly(A[i - 2], A[i - 1], A[i + 1])) {
@@ -36,9 +37,6 @@ public class Plant {
             } else {
                 return -1;
             }
-            // }
-
-            // }
             i++;
         }
 
@@ -53,5 +51,36 @@ public class Plant {
         } else {
             return false;
         }
+    }
+
+    public static int[] slopeDirection(int[] nums) {
+        int[] result = new int[nums.length];
+        int current = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= current) {
+                result[i] = 1;
+            } else {
+                result[i] = 0;
+            }
+            current = nums[i];
+        }
+        return result;
+    }
+
+    public static int solution1(int[] nums) {
+        System.out.println(Arrays.toString(nums));
+        int[] result = slopeDirection(nums);
+        System.out.println(Arrays.toString(result));
+        int peak = 0;
+        int current = result[0];
+        for (int i = 1; i < result.length; i++) {
+            if (current > result[i]) {
+                peak++;
+            }
+            current = result[i];
+        }
+        if (result[result.length - 1] == 1)
+            peak++;
+        return peak;
     }
 }
